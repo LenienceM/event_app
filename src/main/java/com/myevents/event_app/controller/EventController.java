28116ac2969ddefeea335a9d1b51efaa1aeb169e
Lenience Moyo <Lenience.moyo@gmail.com>
2025-07-23 15:42:10 +0200

    no changes, just deleted a blank line testing commit

--- src/main/java/com/myevents/event_app/controller/EventController.java
+++ src/main/java/com/myevents/event_app/controller/EventController.java
@@ -47,7 +47,6 @@ public class EventController {
     public ResponseEntity<Event> deleteEvent(@PathVariable int id) {
         eventService.deleteEvent(id);
         return ResponseEntity.noContent().build();
-
     }
 
 }
